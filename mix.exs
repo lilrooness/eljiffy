2316173c5e55c2268f66b4fbeb4a1dd0adc991da
Joseph Frangoudes <joseph.frangoudes@erlang-solutions.com>
2018-02-28 15:20:42 +0000

    add package and pin to jiffy tag 0.15.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Eljiffy.MixProject do
       version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
